dd5fb74d3f460ca74236c53c85048736b6bf1971
fnoopv <fnoopv@outlook.com>
2025-03-04 17:30:37 +0800

    user_login_log temp

--- service/service.go
+++ service/service.go
@@ -3,4 +3,5 @@ package service
 const (
 	User         = "user"
 	Organization = "organization"
+	UserLoginLog = "user_login_log"
 )
---
+++ service/user_login_log/user_login_log.go
@@ -0,0 +1,33 @@
+package userloginlog
+
+import (
+	"context"
+
+	"github.com/fnoopv/amp/database/model"
+	"github.com/fnoopv/amp/dto"
+	"goyave.dev/filter"
+	"goyave.dev/goyave/v5/database"
+	"goyave.dev/goyave/v5/util/errors"
+	"goyave.dev/goyave/v5/util/typeutil"
+)
+
+type Repository interface {
+	Paginate(ctx context.Context, request *filter.Request) (*database.Paginator[*model.UserLoginLog], error)
+	Create(ctx context.Context, record *model.UserLoginLog) error
+}
+
+type Service struct {
+	userLoginLogRepository Repository
+}
+
+func NewService(userLoginLogRepository Repository) *Service {
+	return &Service{
+		userLoginLogRepository: userLoginLogRepository,
+	}
+}
+
+func (se *Service) Paginate(ctx context.Context, request *filter.Request) (*database.PaginatorDTO[*dto.UserLoginLog], error) {
+	paginator, err := se.userLoginLogRepository.Paginate(ctx, request)
+
+	return typeutil.MustConvert[*database.PaginatorDTO[*dto.UserLoginLog]](paginator), errors.New(err)
+}
